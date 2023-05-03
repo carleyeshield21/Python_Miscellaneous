32de5cd2c31eac492ad101a48df22e8196f1ef0c
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-05-03 11:32:43 +0800

    Update nato_exception.py

--- nato_exception.py
+++ nato_exception.py
@@ -2,14 +2,19 @@ import pandas
 nato = pandas.read_csv('nato_phonetic_alphabet.csv')
 
 # MY SOLUTION
+# nato_iterrows is exactly the same as word_dict, both of these are dictionary
 nato_iterrows = {row.letter:row.code for(index,row) in nato.iterrows()}
 word_dict = {key:value for (key, value) in nato_iterrows.items()}
+print(nato_iterrows)
+print(word_dict)
+print(nato_iterrows == word_dict)
 
 def my_solution():
     word = input('Type any word\n')
     letters_of_word = [letter.upper() for letter in word]
     try:
-        list_output = [word_dict[n] for  n in letters_of_word]
+        # list_output = [word_dict[n] for  n in letters_of_word]
+        list_output = [nato_iterrows[n] for n in letters_of_word]
     except KeyError:
         print('Letters only, no numbers, no punctuation marks, no special characters')
         my_solution()
@@ -18,6 +23,18 @@ def my_solution():
 my_solution()
 
 # dick = {}
+# word = input('Type any word\n')
+# letters_of_word = word.upper()
+# for n in letters_of_word:
+#     print(n)
+#     for key, value in nato_iterrows.items():
+#         if n == key:
+#             dick[key]=value
+# print(dick)
+
+# dick = {}
+# word = input('Type any word\n')
+# letters_of_word = word.upper()
 # for n in letters_of_word:
 #     print(n)
 #     for key, value in nato_iterrows.items():
