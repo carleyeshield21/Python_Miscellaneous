f30ab604ef30eda0685446b0305bf3ec291541e8
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-05-12 17:07:05 +0800

    Update starbucks_df_dict.py

--- starbucks_df_dict.py
+++ starbucks_df_dict.py
@@ -4,15 +4,22 @@ file = pandas.read_csv('python_files/starbucks.csv')
 file_to_dict = file.to_dict(orient='records')
 # file_to_dict = file.to_dict()
 
-for temi in file_to_dict:
-    print(f'item: {temi["item"]}')
-    print(f'calories: {temi["calories"]}')
-    print(temi)
-    print('==============')
-    # print(type(temi))
+# for temi in file_to_dict:
+#     print(f'item: {temi["item"]}')
+#     print(f'calories: {temi["calories"]}')
+#     print(temi)
+#     print('==============')
+#     print(type(temi))
 
-# for every_item in file_to_dict:
-#     print(f'{every_item["item"]}:{every_item["calories"]}')
+# We can create a new dictionary from a dataframe
+# new_dict = {row["item"]:row["calories"] for(index, row) in file.iterrows()}
+# print(new_dict)
+# print(len(new_dict))
 
-# nato_iterrows = {row.letter:row.code for(index,row) in nato.iterrows()}
-# {new_key:new_value for(index, row) in df.iterrows()}
+# for each_item in new_dict.items():
+#     print(each_item[0])
+#     print(each_item[1])
+
+# This code will iterate on each column
+for each in file.items():
+    print(each)
