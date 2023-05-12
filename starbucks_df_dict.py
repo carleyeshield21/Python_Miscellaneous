92f6c22bfe3ec958035e197657cd703e1140bfd5
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-05-12 16:49:34 +0800

    Update starbucks_df_dict.py

--- starbucks_df_dict.py
+++ starbucks_df_dict.py
@@ -1,5 +1,5 @@
 import pandas
-
+# We must import the pandas module to read the csv file by using pandas.read_csv, this file will automatically become a dataframe, then we can convert it to a dictionary by using the method .to_dict. We then pass in the orient argument then set it to records to have it read line per line in each row
 file = pandas.read_csv('python_files/starbucks.csv')
 file_to_dict = file.to_dict(orient='records')
 # file_to_dict = file.to_dict()
@@ -7,6 +7,7 @@ file_to_dict = file.to_dict(orient='records')
 for temi in file_to_dict:
     print(f'item: {temi["item"]}')
     print(f'calories: {temi["calories"]}')
+    print(temi)
     print('==============')
     # print(type(temi))
 
