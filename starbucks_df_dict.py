0bea8c320e5fe69f8b4fd5c8315681b78c216120
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-05-13 12:52:58 +0800

    Update starbucks_df_dict.py

--- starbucks_df_dict.py
+++ starbucks_df_dict.py
@@ -12,14 +12,26 @@ file_to_dict = file.to_dict(orient='records')
 #     print(type(temi))
 
 # We can create a new dictionary from a dataframe
-# new_dict = {row["item"]:row["calories"] for(index, row) in file.iterrows()}
+# new_dict = {row["item"]:row["type"] for(index, row) in file.iterrows()}
 # print(new_dict)
 # print(len(new_dict))
+# print(type(new_dict))
+
+# for key, value in new_dict.items():
+#     print(f'{key} : {value}')
+
+# for items in new_dict.items():
+#     print(items)
+#     print(len(items))
 
 # for each_item in new_dict.items():
 #     print(each_item[0])
 #     print(each_item[1])
 
 # This code will iterate on each column
-for each in file.items():
-    print(each)
+# for each in file.items():
+#     print(each)
+
+# This will create a dictionary of each item with its fat content
+# fat_dict = {row["item"]:row["fat"] for(index, row) in file.iterrows()}
+# print(fat_dict)
