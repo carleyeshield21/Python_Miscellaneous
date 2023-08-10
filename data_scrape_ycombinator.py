6ae6133dfd1472d86fae1a8b910c9ce8b7fa4bb9
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-08-10 13:39:38 +0800

    Update data_scrape_ycombinator.py

--- data_scrape_ycombinator.py
+++ data_scrape_ycombinator.py
@@ -9,21 +9,21 @@ response = requests.get('https://news.ycombinator.com/news').text
 the_soap = BeautifulSoup(response, 'html.parser')
 # print(the_soap.prettify())
 
-# articles = the_soap.find_all(name='span', class_='titleline')
-# item = articles[0]
-# print(item)
-# print(item.get_text())
+# extracting only the valid links in the bs4 Tag
+articles = the_soap.find_all(name='a')
+for arts in articles:
+    the_link = arts.get('href')
+    # print(the_link)
+    if 'https://' in the_link:
+        print(the_link)
 
 # sample of extracting a specific piece of data, here we extract the score
-articles1 = the_soap.find_all(name='span', class_='score')
-print(articles1[0].get_text())
+# articles1 = the_soap.find_all(name='span', class_='score')
+# print(articles1[0].get_text())
 
 # looping through each item, then using the split to further extract the numerical data using the the int to convert
 # from string into an integer value
-for art in articles1:
-    print(art.text)
-    print(art.text.split(' ')[0])
-    print(int(art.text.split(' ')[0]))
-
-# the_text = the_soap.find(name='span',class_='score')
-# print(the_text)s
+# for art in articles1:
+#     print(art.text)
+#     print(art.text.split(' ')[0])
+#     print(int(art.text.split(' ')[0]))
