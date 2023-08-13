4fe4c6d5ba01827798ebb0ea0b1bea73799b53ed
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-08-13 18:12:04 +0800

    Update music_time_machine.py

--- music_time_machine.py
+++ music_time_machine.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import random
 # # billboard_url = 'https://www.billboard.com/charts/hot-100/#'
 billboard_url = 'https://www.billboard.com/charts/hot-100/1980-07-03/'
 response_text = requests.get(billboard_url).text
@@ -12,8 +13,10 @@ song_list = [song.getText().strip() for song in song_title]
 
 song_artist = response_soup.find_all(name='span', class_='a-no-trucate')
 artists_names = [artist.get_text().strip() for artist in song_artist]
-print(artists_names)
 
+# student_scores = {student:random.randint(1,100) for student in names}
+for n in range(0,100):
+    print(f'{song_list[n]} -- {artists_names[n]}')
 
 # # date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 # # URL = "https://www.billboard.com/charts/hot-100/"
