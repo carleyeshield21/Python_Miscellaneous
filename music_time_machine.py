36b904fd9dca5a4ed7304d06bd3f20edd5553d50
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-08-22 12:44:47 +0800

    Update music_time_machine.py

--- music_time_machine.py
+++ music_time_machine.py
@@ -28,3 +28,43 @@ for n in range(0,100):
 # song_title = [song.getText().strip() for song in song_name]
 # for song in song_title:
 #     print(song)
+
+# ==============================================================================
+
+# import requests
+# from bs4 import BeautifulSoup
+
+# # chosen_date = input('Choose a date in this format: YYYY-MM-DD\n')
+# # billboard_url = 'https://www.billboard.com/charts/hot-100/' + chosen_date
+# billboard_url = 'https://www.billboard.com/charts/hot-100/1990-12-25'
+# response_text = requests.get(billboard_url).text
+# response_soup = BeautifulSoup(response_text,'html.parser')
+
+# peak = response_soup.select('li ul li span', class_=['o-chart-results-list__item'])
+# list = [pos.get_text().strip() for pos in peak]
+# wks_on_chart = []
+# n = 3
+# for num in range(n,103):
+#     wks_on_chart.append(int(list[n]))
+#     n+=7
+
+# peak_pos = []
+# m= 2
+# for num in range(m,102):
+#     peak_pos.append(int(list[m]))
+#     m+=7
+
+# print(wks_on_chart)
+# print(peak_pos)
+
+
+# # ===============================================================
+# song_title = response_soup.find_all(name='h3', class_='a-no-trucate')
+# song_list = [song.getText().strip() for song in song_title]
+
+# song_artist = response_soup.find_all(name='span', class_='a-no-trucate')
+# artists_names = [artist.get_text().strip() for artist in song_artist]
+
+# # student_scores = {student:random.randint(1,100) for student in names}
+# for n in range(0,100):
+#     print(f'{n+1}. {song_list[n]} -- {artists_names[n]}')
