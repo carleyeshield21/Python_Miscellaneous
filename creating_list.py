033a85745307021adf61d53b7ed4f4b749a3196b
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-08-30 13:59:36 +0800

    Add files via upload

---
+++ creating_list.py
@@ -0,0 +1,31 @@
+listahan = []
+while True:
+    user_action = input("Type 'a' to add item to list\n's' to view your list\n'e' to edit your list\n'x' to "
+                        "exit and view your list\n")
+    match user_action:
+        case 'a':
+            item = input('Type an entry\n')
+            listahan.append(item.capitalize())
+        case 'x':
+            print(listahan)
+            exit()
+        case 's':
+            for entry in listahan:
+                print(f'{listahan.index(entry)+1}. {entry}')
+            print('==============')
+        case 'e':
+            try:
+                for entry in listahan:
+                    print(f'{listahan.index(entry)+1}. {entry}')
+                to_edit = int(input('Type the number of the item you want to change\n'))
+                if to_edit <= len(listahan):
+                        edited_item = input('Type the new item\n')
+                        listahan[to_edit - 1] = edited_item.capitalize()
+                else:
+                    print('The item you chose does not exist\nPlease try again.\n')
+                    user_action = input("Type 'a' to add item to list\n's' to view your list\n' 'x' to exit and show your list\n'e' to "
+                                        "edit your list\n")
+            except IndexError:
+                print('The item you chose does not exist\nPlease try again.\n')
+                user_action = input("Type 'a' to add item to list\n's' to view your list\n' 'x' to exit and show your list\n'e' to "
+                                    "edit your list\n")
