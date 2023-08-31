566de667777fb98e70217e17a3b0a4c4f9a55bb9
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-08-31 09:11:45 +0800

    Update creating_list.py

--- creating_list.py
+++ creating_list.py
@@ -1,7 +1,7 @@
 listahan = []
 while True:
     user_action = input("Type 'a' to add item to list\n's' to view your list\n'e' to edit your list\n'x' to "
-                        "exit and view your list\n'r' to remove item from the list\n")
+                        "exit and view your list\n'r' to remove item from the list\n").strip().lower()
     match user_action:
         case 'a':
             item = input('Type an entry\n')
@@ -11,14 +11,17 @@ while True:
             exit()
         case 's':
             for entry in listahan:
-                print(f'{listahan.index(entry)+1}. {entry}')
+                print(f'{listahan.index(entry) + 1}. {entry}')
             print('==============')
-
         case 'r':
             num_item = int(input('Type the number of the item you want to remove\n'))
-            listahan.remove(listahan[num_item-1])
-            print(listahan)
-
+            if num_item <= len(listahan):
+                listahan.remove(listahan[num_item - 1])
+                print(listahan)
+            else:
+                print('The item you chose does not exist\nPlease try again.\n')
+                user_action = input("Type 'a' to add item to list\n's' to view your list\n'e' to edit your list\n'x' to "
+                                    "exit and view your list\n'r' to remove item from the list\n").strip().lower()
         case 'e':
             try:
                 for entry in listahan:
@@ -29,9 +32,9 @@ while True:
                         listahan[to_edit - 1] = edited_item.capitalize()
                 else:
                     print('The item you chose does not exist\nPlease try again.\n')
-                    user_action = input("Type 'a' to add item to list\n's' to view your list\n' 'x' to exit and show your list\n'e' to "
-                                        "edit your list\n")
+                    user_action = input("Type 'a' to add item to list\n's' to view your list\n'e' to edit your list\n'x' to "
+                                        "exit and view your list\n'r' to remove item from the list\n").strip().lower()
             except IndexError:
                 print('The item you chose does not exist\nPlease try again.\n')
-                user_action = input("Type 'a' to add item to list\n's' to view your list\n' 'x' to exit and show your list\n'e' to "
-                                    "edit your list\n")
+                user_action = input("Type 'a' to add item to list\n's' to view your list\n'e' to edit your list\n'x' to "
+                                    "exit and view your list\n'r' to remove item from the list\n").strip().lower()
