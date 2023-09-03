cbf7149d04306bd8c52dbcc1f601027a9724afff
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-09-03 12:37:13 +0800

    Update list_with_edit.py

--- list_with_edit.py
+++ list_with_edit.py
@@ -5,22 +5,27 @@ while True:
                         "exit and view your list\n'r' to remove item from the list\n").strip().lower()
     match user_action:
         case 'a':
-            item = input('Type and entry\n') + '\n'
+            item = input('Type an entry\n') + '\n'
             text_file = open('file.txt','r')
             listahan = text_file.readlines()
             listahan.append(item.capitalize())
             text_file = open('file.txt','w')
             text_file.writelines(listahan)
         case 'x':
-            print('The items on you list are:')
+            text_file = open('file.txt','r')
+            listahan = text_file.readlines()
             for i in listahan:
                 print(i.strip())
             exit()
         case 's':
+            text_file = open('file.txt','r')
+            listahan = text_file.readlines()
             for entry in listahan:
                 print(f'{listahan.index(entry) + 1}. {entry.strip()}')
             print('==============')
         case 'r':
+            text_file = open('file.txt', 'r')
+            listahan = text_file.readlines()
             num_item = int(input('Type the number of the item you want to remove\n'))
             if num_item <= len(listahan):
                 listahan.remove(listahan[num_item - 1])
@@ -31,12 +36,26 @@ while True:
                                     "exit and view your list\n'r' to remove item from the list\n").strip().lower()
         case 'e':
             try:
+                text_file = open('file.txt','r')
+                listahan = text_file.readlines()
+                print(listahan)
                 for entry in listahan:
                     print(f'{listahan.index(entry)+1}. {entry.strip()}')
                 to_edit = int(input('Type the number of the item you want to change\n'))
+                print(to_edit)
+                print(type(to_edit))
                 if to_edit <= len(listahan):
-                        edited_item = input('Type the new item\n')
+                        edited_item = input('Type the new entry\n') + '\n'
+                        text_file = open('file.txt','r')
                         listahan[to_edit - 1] = edited_item.capitalize()
+
+                        print(listahan)
+                        text_file = open('file.txt','w')
+                        text_file.writelines(listahan)
+
+                        print(f'Length of listahan{len(listahan)}')
+
+
                 else:
                     print('The item you chose does not exist\nPlease try again.\n')
                     user_action = input("Type 'a' to add item to list\n's' to view your list\n'e' to edit your list\n'x' to "
