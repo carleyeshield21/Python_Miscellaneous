d9cec0fd3b66e4bf1134edd0295912bd0bf8e8bf
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-10-08 12:02:51 +0800

    Update python_pdf.py

--- python_pdf.py
+++ python_pdf.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import re
 
 with open("MikeMaloney.pdf", 'rb') as pdf:
     reader = PyPDF2.PdfReader(pdf, strict=False)
@@ -8,6 +9,15 @@ with open("MikeMaloney.pdf", 'rb') as pdf:
         content = page.extract_text()
         pdf_text.append(content)
 
-print(pdf_text)
-print(type(pdf_text))
-print(len(pdf_text))
+pdf_text_to_string = str(pdf_text)
+
+# pattern = re.compile("[^.]* gold [^.]*.")
+pattern = re.compile("[^.]* gold [^.?]*.")
+findings = re.findall(pattern, pdf_text_to_string)
+# for item in findings:
+#     print(item.replace('\\n',''))
+#     print(type(item))
+
+sentences_with_gold = [item.replace('\\n','') for item in findings]
+print(sentences_with_gold)
+print(len(sentences_with_gold))
