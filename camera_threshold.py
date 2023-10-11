7a728bbc5642628928b9e3bb2f8572d9d33cf3eb
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-10-11 12:36:22 +0800

    Add files via upload

---
+++ camera_threshold.py
@@ -0,0 +1,32 @@
+import cv2
+import time
+
+bidyo = cv2.VideoCapture(1)
+time.sleep(1)
+
+if not bidyo.isOpened():
+   print("Ayaw mabuksan ang camera, shet")
+   exit()
+
+pers_frame = None
+
+while True:
+    check, frame = bidyo.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gaussian = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+
+    if pers_frame is None:
+        pers_frame = gray_frame_gaussian
+
+    delta_frame = cv2.absdiff(pers_frame, gray_frame_gaussian)
+    threshold_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow('My bidyo',threshold_frame)
+    print(delta_frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+bidyo.release()
