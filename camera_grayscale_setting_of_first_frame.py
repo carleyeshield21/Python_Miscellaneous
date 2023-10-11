b1eb437be907aecc77b4677d5ff523b8f3e497de
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-10-11 12:11:57 +0800

    Add files via upload

---
+++ camera_grayscale_setting_of_first_frame.py
@@ -0,0 +1,27 @@
+import cv2
+import time
+
+bidyo = cv2.VideoCapture(1)
+time.sleep(1)
+
+if not bidyo.isOpened():
+   print("Ayaw mabuksan ang camera, shet")
+   exit()
+
+pers_frame = None
+
+while True:
+    check, frame = bidyo.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gaussian = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+    cv2.imshow('My bidyo', gray_frame_gaussian)
+
+    if pers_frame is None:
+        pers_frame = gray_frame_gaussian
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+bidyo.release()
