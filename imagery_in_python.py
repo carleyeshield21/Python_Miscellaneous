59d4a61291fb7b23f3b83a12a3dc7091275221b0
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-10-11 11:39:39 +0800

    Update imagery_in_python.py

--- imagery_in_python.py
+++ imagery_in_python.py
@@ -13,9 +13,3 @@ while True:
     if key == ord('q'):
         break
 bidyo.release()
-
-# img_array = cv2.imread('profile_pic.png')
-# print(len(img_array))
-# print(img_array.shape)
-# for arr in img_array:
-#     print(len(arr))
