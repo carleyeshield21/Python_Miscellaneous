4c54b1b9431e25e144dfcddb4f11c50383bd7501
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2023-10-11 13:03:47 +0800

    Add files via upload

---
+++ camera_contour_area.py
@@ -0,0 +1,43 @@
+import cv2
+import time
+
+bidyo = cv2.VideoCapture(1)
+time.sleep(1)
+
+if not bidyo.isOpened():
+   print("Ayaw mabuksan ang camera, shet")
+   exit()
+
+pers_frame = None
+
+while True:
+    check, frame = bidyo.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gaussian = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+
+    if pers_frame is None:
+        pers_frame = gray_frame_gaussian
+
+    delta_frame = cv2.absdiff(pers_frame, gray_frame_gaussian)
+    threshold_frame = cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)[1]
+    dilated_frame = cv2.dilate(threshold_frame, None, iterations=2)
+    cv2.imshow('My bidyo',dilated_frame)
+    contours, check = cv2.findContours(dilated_frame,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+
+    cv2.imshow('bidyo2', frame)
+
+    print(delta_frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+bidyo.release()
